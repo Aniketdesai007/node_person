d196d930018e9dbc132877b48cd7aab9a69c6d2c
aniketdesai <aniketdesai1570@gmail.com>
2024-06-19 23:35:13 +0530

    added new port

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use('/per',personrouter);
 
 const Port=process.env.port || 3000;
 app.listen(Port,()=>{
-    console.log('listening on port 800');
+    console.log('listening on port 3000');
 });
